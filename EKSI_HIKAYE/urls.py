86a4f8c5945d35e7d6ad91c0ac34244d2ec5bdab
oznurf <oznur.filizoglu@hotmail.com>
2015-08-20 21:13:14 +0300

    Initial commit

--- EKSI_HIKAYE/urls.py
+++ EKSI_HIKAYE/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from hikaye.views import (index, register_user, login_user, logout_user)
+
 urlpatterns = [
+    url(r'^$', index, name='home'),
+    url(r'^register$', register_user, name='register'),
+    url(r'^login', login_user, name='login'),
+    url(r'^logout', logout_user, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- hikaye/views.py
+++ hikaye/views.py
@@ -1,3 +1,51 @@
-+3from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from .models import Story
+from hikaye.forms import MyUserCreationForm
 
-# Create your views here.
+
+def index(request):
+    if 'title' in request.GET:
+        objects = Story.objects.filter(
+            title__icontains=request.GET['title'])
+    else:
+        objects = Story.objects.all()
+    return render(request, 'index.html',
+                  {'objects': objects})
+
+
+
+def register_user(request):
+    form = MyUserCreationForm()
+
+    if request.method == "POST":
+        form = MyUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    return render(request, 'register.html', {
+        "form": form
+    })
+
+
+def login_user(request):
+    form = AuthenticationForm()
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/')
+
+    return render(request, 'login.html', {
+        "form": form
+    })
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
